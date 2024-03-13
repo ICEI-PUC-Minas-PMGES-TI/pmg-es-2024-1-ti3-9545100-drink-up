1c9276a65a3a07cffebe475c166011a5a79e28a6
JVTemponi <jvictortemponi@gmail.com>
2024-03-13 01:01:44 -0300

    Arquivo para exportar os models mais facilmente

---
+++ Codigo/backend/models/models.js
@@ -0,0 +1,19 @@
+const Usuario = require('./Usuario');
+const Cliente = require('./Cliente');
+const Fornecedor = require('./Fornecedor');
+const Produto = require('./Produto');
+const Telefone = require('./Telefone');
+const Endereco = require('./Endereco');
+const Imagem = require('./Imagem');
+const Categoria = require('./Categoria');
+
+module.exports = {
+  Usuario,
+  Cliente,
+  Fornecedor,
+  Produto,
+  Telefone,
+  Endereco,
+  Imagem,
+  Categoria
+};
