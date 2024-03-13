7f07b6279b6178bafbc19b75ba948d8fa2cb9bb1
JVTemponi <jvictortemponi@gmail.com>
2024-03-13 00:59:56 -0300

    Model de categoria

---
+++ Codigo/backend/models/Categoria.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require('sequelize');
+const Database = require('../Database');
+
+class Categoria extends Model {}
+
+Categoria.init(
+  {
+    descricao: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    }
+  },
+  {
+    sequelize: Database.sequelize,
+    modelName: 'Categoria',
+    tableName: 'tb_categoria',
+    timestamps: false
+  }
+);
+
+module.exports = Categoria;
