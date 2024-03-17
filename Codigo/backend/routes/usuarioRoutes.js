ed467904dc87042b9defe34105096085cf124d34
JVTemponi <jvictortemponi@gmail.com>
2024-03-17 17:23:59 -0300

    ajustando totas de usuario

--- Codigo/backend/routes/usuarioRoutes.js
+++ Codigo/backend/routes/usuarioRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/usuarios', usuarioController.criarUsuario);
 router.get('/usuarios', usuarioController.listarTodosUsuarios);
 router.get('/usuarios/:id', usuarioController.buscarUsuarioPorId);
+router.get('/usuarios/email/:email', usuarioController.buscarUsuarioPorEmail);
 router.put('/usuarios/:id', usuarioController.atualizarUsuario);
 router.delete('/usuarios/:id', usuarioController.excluirUsuario);
 
