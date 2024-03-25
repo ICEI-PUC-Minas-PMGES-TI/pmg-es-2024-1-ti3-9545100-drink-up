24c3e1791c88d28d57f35b62e84c36e8422e6074
Henriquelobo5 <neveshenrique050@gmail.com>
2024-03-25 12:15:11 -0300

    Style: pagina Listagem de Bebidas

---
+++ Codigo/frontend/js/ListagemDeBebidas.js
@@ -0,0 +1,20 @@
+// Seleciona o botão "Editar"
+const botaoEditar = document.querySelector('.editar');
+
+// Seleciona o popup
+const popup = document.getElementById('popup');
+
+// Adiciona evento de clique ao botão "Editar"
+botaoEditar.addEventListener('click', function() {
+    // Exibe o popup
+    popup.style.display = 'block';
+});
+
+// Seleciona o botão "Fechar" dentro do popup
+const botaoFecharPopup = document.getElementById('fecharPopup');
+
+// Adiciona evento de clique ao botão "Fechar"
+botaoFecharPopup.addEventListener('click', function() {
+    // Oculta o popup
+    popup.style.display = 'none';
+});
