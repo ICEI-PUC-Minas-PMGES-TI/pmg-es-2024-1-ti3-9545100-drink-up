ca61d492a5a279cd6e2b69fe8ec129a94e7407b4
JVTemponi <jvictortemponi@gmail.com>
2024-03-29 21:12:55 -0300

    Correção do middleware de verificação do token gerado - JWT

--- Codigo/backend/middlewares/autenticacao.js
+++ Codigo/backend/middlewares/autenticacao.js
@@ -1,16 +1,21 @@
 const jwt = require('jsonwebtoken');
+const SECRET = 'drinkupTIS3';
 
 
 const autenticacao = (req, res, next) => {
-    
-    console.log('Token recebido:', req.headers.authorization);
-    //Verifica se o usuário está autenticado
-    if (req.session.usuario) {
-      next(); // Permite o acesso à próxima função (rota)
-    } else {
-      res.status(401).json({ error: 'Não autorizado' }); // Retorna erro se não estiver autenticado
+
+  console.log('Token recebido:', req.headers.authorization);
+  //Verifica se o usuário está autenticado
+  const token = req.headers['x-access-token'];
+  jwt.verify(token, SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).end();
     }
-  };
-  
-  module.exports = autenticacao;
-  
+    req.id = decoded.id;
+    req.perfil = decoded.perfil;
+    req.status = decoded.status;
+    next();
+  })
+};
+
+module.exports = autenticacao;
