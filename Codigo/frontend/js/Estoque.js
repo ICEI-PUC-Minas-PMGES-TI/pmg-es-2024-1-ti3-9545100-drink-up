060a9dcea6e88844a7c6246e327c73bbeeedef38
Ian Matsuhara <86268101+YoshuJumpMaster@users.noreply.github.com>
2024-04-15 17:51:16 -0300

    Update Estoque.js

--- Codigo/frontend/js/Estoque.js
+++ Codigo/frontend/js/Estoque.js
@@ -44,36 +44,46 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => console.error('Erro ao puxar produtos do banco', error));
           });
 
-    function addEventListenersToButtons() {
-        const addButtons = document.querySelectorAll('.adicionar');
-        const removeButtons = document.querySelectorAll('.remover');
+                          
+      // botoões
 
-        addButtons.forEach(button => {
-            button.addEventListener('click', function(event) {
-                const tr = event.target.closest('tr');
-                const quantidadeTd = tr.querySelector('td:nth-child(3)');
-                let quantidadeAtual = parseInt(quantidadeTd.textContent);
-                quantidadeAtual = quantidadeAtual + 1; 
-                quantidadeTd.textContent = quantidadeAtual;
-            });
+   function addEventListenersToButtons() {
+    const adicionarButtons = document.querySelectorAll('.adicionar');
+    const removerButtons = document.querySelectorAll('.remover');
+    const novoProdutoButton = document.querySelector('.button-NovoProduto');
+
+    adicionarButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            const tr = event.target.closest('tr');
+            const tdQuantidade = tr.querySelector('td:nth-child(3)');
+            let quantidade_produto = parseInt(prompt("Quantos itens queremos adicionar?"));
+            if (quantidade_produto) {
+                tdQuantidade.textContent = parseInt(tdQuantidade.textContent) + quantidade_produto;
+                console.log(`foram adicionados ${quantidade_produto} itens ao ${tr.querySelector('td:nth-child(1)').textContent}.`);
+            }
+            else{
+                 alert('valor inválido digitado. Favor tentar novamente');
+            }
         });
+    });
 
-
-        
-
-        removeButtons.forEach(button => {
-            button.addEventListener('click', function(event) {
-                const tr = event.target.closest('tr');
-                const quantidadeTd = tr.querySelector('td:nth-child(3)');
-                let quantidadeAtual = parseInt(quantidadeTd.textContent);
-                if (quantidadeAtual > 0) {
-                    quantidadeAtual = quantidadeAtual - 1; 
-                    quantidadeTd.textContent = quantidadeAtual;
+    removerButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            const tr = event.target.closest('tr');
+            const tdQuantidade = tr.querySelector('td:nth-child(3)');
+            let quantidade_produto = parseInt(prompt("Quantos itens queremos remover?"));
+            if (quantidade_produto) {
+                if (parseInt(tdQuantidade.textContent) >= quantidade_produto) {
+                    tdQuantidade.textContent = parseInt(tdQuantidade.textContent) - quantidade_produto;
+                    console.log(`Foram removidos ${quantidade_produto} itens de ${tr.querySelector('td:nth-child(1)').textContent}.`);
                 } else {
-                    alert('operação inválida. Tente novamente!');
+                    alert('Quantidade insuficinete para esta operação :(   tente novamente');
                 }
-            });
+            }
+            else{
+                alert('valor inválido digitado. Favor tentar novamente');
+            }
         });
+    });
     }
 });
-
