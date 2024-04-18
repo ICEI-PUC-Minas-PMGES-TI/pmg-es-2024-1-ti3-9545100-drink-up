3d7e661665e7eb16634fbfc3854d262900498651
JVTemponi <jvictortemponi@gmail.com>
2024-04-18 00:33:33 -0300

    Correção do middleware para utilização do token

--- Codigo/backend/middlewares/autenticacao.js
+++ Codigo/backend/middlewares/autenticacao.js
@@ -2,27 +2,33 @@ const jwt = require('jsonwebtoken');
 const SECRET = 'drinkupTIS3';
 
 const autenticacao = (requerAutenticacao) => (req, res, next) => {
-  // Se a rota não exigir autenticação, permitir o acesso sem token
-  if (!requerAutenticacao) {
-    return next();
-  }
 
-  // Verificação do token
-  const token = req.headers['x-access-token'];
-  if (!token) {
-    return res.status(401).end(); // Token não fornecido
-  }
+  if (requerAutenticacao) {
 
-  jwt.verify(token, SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).end(); // Token inválido
+    const token = req.headers['x-access-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Token não fornecido' });
     }
-    req.id = decoded.id;
-    req.perfil = decoded.perfil;
-    req.status = decoded.status;
+
+    jwt.verify(token, SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Token inválido' });
+      }
+      
+      
+      req.id = decoded.id;
+      req.perfil = decoded.perfil;
+      req.status = decoded.status;
+
+      next();
+    });
     
+  }
+  else{
     next();
-  });
+  }
+
 };
 
 module.exports = autenticacao;
