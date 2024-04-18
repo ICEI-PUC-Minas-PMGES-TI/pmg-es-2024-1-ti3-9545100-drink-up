48839af57104e70443d5370275c0c81e9b094cc9
JVTemponi <jvictortemponi@gmail.com>
2024-04-18 00:56:19 -0300

    Ajuste routes usuário

--- Codigo/backend/routes/usuarioRoutes.js
+++ Codigo/backend/routes/usuarioRoutes.js
@@ -9,10 +9,10 @@ router.post('/login', autenticacao(false), usuarioController.login);
 router.post('/usuarios', autenticacao(false), usuarioController.criarUsuario);
 
 // Rotas com autenticação
-router.get('/usuarios', autenticacao(true) ,usuarioController.listarTodosUsuarios);
-router.get('/usuarios/:id', autenticacao(false), usuarioController.buscarUsuarioPorId);
-router.get('/usuarios/email/:email', autenticacao(true), usuarioController.buscarUsuarioPorEmail);
-router.put('/usuarios/:id', autenticacao(false), usuarioController.atualizarUsuario);
-router.delete('/usuarios/:id', autenticacao(false), usuarioController.excluirUsuario);
+router.get('/usuarios', autenticacao(true, 'admin') ,usuarioController.listarTodosUsuarios);
+router.get('/usuarios/:id', autenticacao(false, 'admin'), usuarioController.buscarUsuarioPorId);
+router.get('/usuarios/email/:email', autenticacao(false, 'admin'), usuarioController.buscarUsuarioPorEmail);
+router.put('/usuarios/:id', autenticacao(false, 'admin'), usuarioController.atualizarUsuario);
+router.delete('/usuarios/:id', autenticacao(false, 'admin'), usuarioController.excluirUsuario);
 
 module.exports = router;
