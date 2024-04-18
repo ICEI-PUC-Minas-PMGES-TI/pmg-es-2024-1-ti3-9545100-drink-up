6972debd7cdc48c01cbe9ae23b7623dd0607462a
JVTemponi <jvictortemponi@gmail.com>
2024-04-18 00:34:56 -0300

    Ajuste das chamadas com token em usuarios

--- Codigo/backend/routes/usuarioRoutes.js
+++ Codigo/backend/routes/usuarioRoutes.js
@@ -5,14 +5,14 @@ const autenticacao = require('../middlewares/autenticacao');
 const router = express.Router();
 
 // Rotas sem autenticação
-router.post('/login', usuarioController.login);
-router.post('/usuarios', usuarioController.criarUsuario);
+router.post('/login', autenticacao(false), usuarioController.login);
+router.post('/usuarios', autenticacao(false), usuarioController.criarUsuario);
 
 // Rotas com autenticação
-router.get('/usuarios', autenticacao(true), usuarioController.listarTodosUsuarios);
-router.get('/usuarios/:id', autenticacao(true), usuarioController.buscarUsuarioPorId);
+router.get('/usuarios', autenticacao(true) ,usuarioController.listarTodosUsuarios);
+router.get('/usuarios/:id', autenticacao(false), usuarioController.buscarUsuarioPorId);
 router.get('/usuarios/email/:email', autenticacao(true), usuarioController.buscarUsuarioPorEmail);
-router.put('/usuarios/:id', autenticacao(true), usuarioController.atualizarUsuario);
-router.delete('/usuarios/:id', autenticacao(true), usuarioController.excluirUsuario);
+router.put('/usuarios/:id', autenticacao(false), usuarioController.atualizarUsuario);
+router.delete('/usuarios/:id', autenticacao(false), usuarioController.excluirUsuario);
 
 module.exports = router;
