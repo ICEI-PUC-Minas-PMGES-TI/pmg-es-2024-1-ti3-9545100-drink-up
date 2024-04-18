2375d0d7b5d4b2703312bb54d0d079930820cecd
Ian Matsuhara <86268101+YoshuJumpMaster@users.noreply.github.com>
2024-04-17 22:52:00 -0300

    Update produtoRoutes.js #74

--- Codigo/backend/routes/produtoRoutes.js
+++ Codigo/backend/routes/produtoRoutes.js
@@ -9,6 +9,7 @@ router.get('/produtos/nome/:nome', produtoController.buscarProdutoPorNome);
 router.get('/produtos/:id', produtoController.buscarProdutoPorId);
 router.get('/produtos', produtoController.listarTodosProdutos);
 router.put('/produtos', produtoController.atualizarProduto);
+router.put('/produtos/atualizar-estoque/:id', produtoController.atualizarEstoqueProduto);
 router.delete('/produtos/:id', produtoController.excluirProduto);
 
 module.exports = router;
