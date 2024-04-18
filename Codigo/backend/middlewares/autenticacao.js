1f04cd46c4fa795243740f5b9e35aab2b1515e98
JVTemponi <jvictortemponi@gmail.com>
2024-04-18 00:56:08 -0300

    Verificação do perfil de acesso e status do usuário

--- Codigo/backend/middlewares/autenticacao.js
+++ Codigo/backend/middlewares/autenticacao.js
@@ -1,27 +1,35 @@
 const jwt = require('jsonwebtoken');
 const SECRET = 'drinkupTIS3';
 
-const autenticacao = (requerAutenticacao) => (req, res, next) => {
+const autenticacao = (requerAutenticacao, permissao) => (req, res, next) => {
 
+  //Verifica se a rota exige requisição. Caso exija, prossegue com as validações
   if (requerAutenticacao) {
 
     const token = req.headers['x-access-token'];
-
+    //Verifica se o token foi fornecido
     if (!token) {
       return res.status(401).json({ error: 'Token não fornecido' });
     }
 
+    //Validação e decodificação do token
     jwt.verify(token, SECRET, (err, decoded) => {
       if (err) {
         return res.status(401).json({ error: 'Token inválido' });
       }
-      
-      
+
       req.id = decoded.id;
       req.perfil = decoded.perfil;
       req.status = decoded.status;
 
-      next();
+      //Verifica se o perfil definido para acesso na rota condiz com o perfil do token gerado
+      if (permissao == req.perfil || !permissao && req.status == 1) {
+        next();
+      }
+      else{
+        return res.status(401).json({ error: 'Usuário inativo ou não possui permissão de acesso' });
+      }
+
     });
     
   }
