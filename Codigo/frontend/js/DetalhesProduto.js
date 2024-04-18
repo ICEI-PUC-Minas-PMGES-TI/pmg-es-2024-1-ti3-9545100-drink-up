86d004f19e07e7d91c96b41e9e795358e3dd3185
Ian Matsuhara <86268101+YoshuJumpMaster@users.noreply.github.com>
2024-04-17 22:26:19 -0300

    Update DetalhesProduto.js #72

--- Codigo/frontend/js/DetalhesProduto.js
+++ Codigo/frontend/js/DetalhesProduto.js
@@ -1,61 +1,60 @@
 document.addEventListener("DOMContentLoaded", function () {
-const fetch = require('node-fetch');
-
-const productId = window.location.search.slice(5).trim();
-
-fetch(`http://localhost:3000/produtos/${productId}`)
-  .then((response) => response.json())
-  .then((produto) => {
-    // Atualize os elementos HTML com os dados do produto
-    document.querySelector('h1').textContent = `${produto.nome} ${produto.tam_garrafa}ml`;
-    document.querySelector('h3').textContent = `R$ ${produto.valor}`;
-    document.querySelector('p').textContent = produto.descricao;
-
-    // Atualize a imagem do produto
-    const productImage = document.querySelector('#imagem img');
-    productImage.src = produto.imagem;
-    productImage.alt = produto.nome;
-  });
-});
+    const productId = new URLSearchParams(window.location.search).get('id');
   
-//Botões
-const value = document.getElementById('value');
-const plusbutton = document.getElementById('plus');
-const minusbutton = document.getElementById('minus');
-const reset = document.getElementById('reset');
-
-const updateValue = () => {
-    value.innerHTML = count;
-};
-
-let count = 0;
-let intervalId = 0;
-
-plusbutton.addEventListener('mousedown', () =>{
-    count += 1;
-    updateValue();
-    intervalId = setInterval(()=> {
+    fetch(`http://localhost:3000/produtos/${productId}`)
+      .then((response) => response.json())
+      .then((produto) => {
+        // Atualize os elementos HTML com os dados do produto
+        document.querySelector('h1').textContent = `${produto.nome} ${produto.tam_garrafa}ml`;
+        document.querySelector('h3').textContent = `R$ ${produto.valor}`;
+        document.querySelector('p').textContent = produto.descricao;
+  
+        // Atualize a imagem do produto
+        const productImage = document.querySelector('#imagem img');
+        productImage.src = produto.imagem;
+        productImage.alt = produto.nome;
+      })
+      .catch(error => console.error('Falha ao buscar detalhes do produto:', error));
+  
+    // Botões 
+    const value = document.getElementById('value');
+    const plusbutton = document.getElementById('plus');
+    const minusbutton = document.getElementById('minus');
+    const reset = document.getElementById('reset');
+  
+    const updateValue = () => {
+      value.innerHTML = count;
+    };
+  
+    let count = 0;
+    let intervalId = 0;
+  
+    plusbutton.addEventListener('mousedown', () => {
+      count += 1;
+      updateValue();
+      intervalId = setInterval(() => {
         count += 1;
         updateValue();
-    }, 100);
-});
-
-minusbutton.addEventListener('mousedown', () =>{
-    if (count > 0) { // Verifica se o contador é maior que zero antes de subtrair
+      }, 100);
+    });
+  
+    minusbutton.addEventListener('mousedown', () => {
+      if (count > 0) { // Verifica se o contador é maior que zero antes de subtrair
         count -= 1;
         updateValue();
-        intervalId = setInterval(()=> {
-            if (count > 0) { // Verifica novamente se o contador é maior que zero antes de subtrair
-                count -= 1;
-                updateValue();
-            }
+        intervalId = setInterval(() => {
+          if (count > 0) { // Verifica novamente se o contador é maior que zero antes de subtrair
+            count -= 1;
+            updateValue();
+          }
         }, 100);
-    }
-});
-
-document.addEventListener('mouseup', () => clearInterval(intervalId));
-
-reset.addEventListener('click', () =>{
-    count = 0; 
-    updateValue();
-})
+      }
+    });
+  
+    document.addEventListener('mouseup', () => clearInterval(intervalId));
+    reset.addEventListener('click', () => {
+      count = 0;
+      updateValue();
+    });
+  });
+  
