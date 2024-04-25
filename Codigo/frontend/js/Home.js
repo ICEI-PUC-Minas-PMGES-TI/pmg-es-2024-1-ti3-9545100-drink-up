6adde8b254ea5a8851a1d7d95d235349aa8052fd
Ian Matsuhara <86268101+YoshuJumpMaster@users.noreply.github.com>
2024-04-25 12:03:41 -0300

    Update Home.js #87

--- Codigo/frontend/js/Home.js
+++ Codigo/frontend/js/Home.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const baseUrl = 'http://localhost:3000';
 
-
-
     
 
     function carregarProdutos(filtroCategoria) {
@@ -17,20 +15,24 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             const productList = document.getElementById('product-list');
             productList.innerHTML = '';
-            filteredProdutos.forEach(product => {
-                const productCard = document.createElement('div');
-                productCard.className = 'product-card';
-                productCard.innerHTML = `
-                    <img src="${product.url_imagem}" alt="${product.nome}" class="product-image" onerror="this.onerror=null; this.src='../../../img/beer.png';">
-                    <div class="product-description">
-                        <h4>${product.nome}</h4>
-                        <p>${product.descricao}</p>
-                        <p class="product-price">R$ ${product.valor ? parseFloat(product.valor).toFixed(2) : 'N/A'}</p>
-                        <button onclick="buyProduct(${product.id})" class="buy-button">Comprar</button>
-                    </div>
-                `;
-                productList.appendChild(productCard);
-            });
+            if (filteredProdutos.length === 0) {
+                productList.innerHTML = '<p>Nenhum produto encontrado para esta categoria.</p>';
+            } else {
+                filteredProdutos.forEach(product => {
+                    const productCard = document.createElement('div');
+                    productCard.className = 'product-card';
+                    productCard.innerHTML = `
+                        <img src="${product.url_imagem}" alt="${product.nome}" class="product-image" onerror="this.onerror=null; this.src='../../../img/beer.png';">
+                        <div class="product-description">
+                            <h4>${product.nome}</h4>
+                            <p>${product.descricao}</p>
+                            <p class="product-price">R$ ${product.valor ? parseFloat(product.valor).toFixed(2) : 'N/A'}</p>
+                            <button onclick="buyProduct(${product.id})" class="buy-button">Comprar</button>
+                        </div>
+                    `;
+                    productList.appendChild(productCard);
+                });
+            }
         })
         .catch(error => console.error('Erro ao carregar produtos:', error));
     }
@@ -54,10 +56,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    document.querySelector('.button-busca').addEventListener('click', function() {
-        const selectedCategoria = document.getElementById('categorySelect').value;
-        carregarProdutos(selectedCategoria);
-    });
+        document.querySelector('.button-busca').addEventListener('click', function() {
+            const selectedCategoria = document.getElementById('categorySelect').value;
+            console.log("Categoria selecionada para filtragem:", selectedCategoria);  
+            carregarProdutos(selectedCategoria);
+        });
+    
 
 
 
