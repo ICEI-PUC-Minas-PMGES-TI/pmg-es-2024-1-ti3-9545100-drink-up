73fd0c7c5593031fda60a57f58612b391466b60b
sophiaamr <100172972+sophiaamr@users.noreply.github.com>
2024-06-07 01:06:06 -0300

    Front inteiro

--- Codigo/frontend/js/carrinho.js
+++ Codigo/frontend/js/carrinho.js
@@ -1,85 +1,55 @@
 import { Carrinho } from "./utils/Carrinho.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const baseUrl = 'http://localhost:3000';
-    const carrinho = new Carrinho();
-    const products = carrinho.carregarProdutosDoCookie();
-    let carrinhoHTML = '';
+  const carrinho = new Carrinho();
 
-    const productList = document.getElementById("carrinho-body");
-    productList.innerHTML = ''; // Limpar lista de produtos inicialmente
+  const products = carrinho.carregarProdutosDoCookie();
 
-    products.forEach((element) => {
-        fetch(`${baseUrl}/imagens/${element.id_imagem}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erro ao buscar a imagem: ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(imageData => {
-                if (!imageData || !imageData.caminho) {
-                    throw new Error('A URL da imagem não está disponível.');
-                }
-                const imageUrl = imageData.caminho;
-                carrinhoHTML += `
-                    <tr>
-                        <td><img src="${imageUrl}" alt="${element.nome}" class="product-image" onerror="this.onerror=null; this.src='../../../img/beer.png';"></td>
-                        <td>${element.nome}</td>
-                        <td id="item-value">R$ ${element.valor}</td>
-                        <td>
-                            <div class="quantity-buttons">
-                                <button id="decrease-button${element.id}"><i class="fa-solid fa-minus"></i></button>
-                                <span class="quantity" id="quantity${element.id}">${element.quant}</span>
-                                <button id="increase-button${element.id}"><i class="fa-solid fa-plus"></i></button>
-                            </div>
-                        </td>
-                        <td>
-                            <button class="remove-button" id="remove-item${element.id}"><i class="fa-solid fa-trash"></i></button>
-                        </td>
-                    </tr>
-                `;
-                productList.innerHTML = carrinhoHTML;
-                addEventListeners(); // Adicionar os listeners após atualizar o HTML
-            })
-            .catch(error => {
-                console.error("Erro ao buscar a imagem:", error);
-                carrinhoHTML += `
-                    <tr>
-                        <td><img src="../../../img/beer.png" alt="${element.nome}" class="product-image"></td>
-                        <td>${element.nome}</td>
-                        <td id="item-value">R$ ${element.valor}</td>
-                        <td>
-                            <div class="quantity-buttons">
-                                <button id="decrease-button${element.id}"><i class="fa-solid fa-minus"></i></button>
-                                <span class="quantity" id="quantity${element.id}">${element.quant}</span>
-                                <button id="increase-button${element.id}"><i class="fa-solid fa-plus"></i></button>
-                            </div>
-                        </td>
-                        <td>
-                            <button class="remove-button" id="remove-item${element.id}"><i class="fa-solid fa-trash"></i></button>
-                        </td>
-                    </tr>
-                `;
-                productList.innerHTML = carrinhoHTML;
-                addEventListeners(); // Adicionar os listeners após atualizar o HTML
-            });
+  let carrinhoHTML = '';
+  products.forEach((element) => {
+    //const imageUrl = element.imagem || '../../../img/beer.png'; // verifica se o element de imagem é nulo. Atualmente não funciona pois os testes de banco está passando valores fictícios. Deverá ser adaptado.
+    const imageUrl = '../../../img/beer.png';
+    carrinhoHTML += `
+        <tr>
+        <td><img src="${imageUrl}" alt="Produto" class="product-image"></td>
+            <td>${element.nome}</td>
+            <td id="item-value">R$ ${element.valor}</td>
+            <td>
+                <div class="quantity-buttons">
+                    <button id="decrease-button${element.id}"><i class="fa-solid fa-minus"></i></button>
+                    <span class="quantity" id="quantity${element.id}">${element.quant}</span>
+                    <button id="increase-button${element.id}"><i class="fa-solid fa-plus"></i></button>
+                </div>
+            </td>
+            <td>
+                <button class="remove-button" id="remove-item${element.id}"><i class="fa-solid fa-trash"></i></button>
+            </td>
+        </tr>
+    `;
+
+  });
+
+  document.getElementById("carrinho-body").innerHTML = carrinhoHTML;
+
+  carrinho.updateCarrinhoTotalValue();
+
+  products.forEach((element) => {
+    carrinho.setupCounter(element.quant, element.id);
+  })
+
+
+
+  
+  let elementsRemoveIcons = document.querySelectorAll('[id^="remove-item"]');
+
+  elementsRemoveIcons.forEach((element) => {
+    element.addEventListener("click", () => {
+      let itemToRemoveId = element.id.replace("remove-item", "");
+
+      carrinho.removerItem(parseInt(itemToRemoveId));
+
+      window.location.reload();
     });
+  });
 
-    carrinho.updateCarrinhoTotalValue();
-
-    products.forEach((element) => {
-        carrinho.setupCounter(element.quant, element.id);
-    });
-
-    function addEventListeners() {
-        let elementsRemoveIcons = document.querySelectorAll('[id^="remove-item"]');
-        elementsRemoveIcons.forEach((element) => {
-            element.addEventListener("click", () => {
-                let itemToRemoveId = element.id.replace("remove-item", "");
-                carrinho.removerItem(parseInt(itemToRemoveId));
-                window.location.reload();
-            });
-        });
-    }
 });
